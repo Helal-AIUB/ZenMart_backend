e9dc9a1b47cbf50acf5a7c4ae5bbb387c74be54e
Helal-AIUB <helalaiub8009@gmail.com>
2026-08-19 11:30:48 +0600

    Update prod.py with dynamic DATABASE_URL

--- storefront/settings/prod.py
+++ storefront/settings/prod.py
@@ -1,7 +1,12 @@
 import os
 from .common import *
+import dj_database_url
 
 DEBUG = False
 SECRET_KEY = os.environ['SECRET_KEY']
 
 ALLOWED_HOSTS = ['zenmart-backend.onrender.com', 'localhost', '127.0.0.1']
+
+DATABASES = {
+    'default': dj_database_url.config()
+}
